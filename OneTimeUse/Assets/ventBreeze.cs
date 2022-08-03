95c34141b1a3222c71852e648e76ca9e8752ed10
Jacob Fifield <43118015+Fife@users.noreply.github.com>
2022-08-02 20:06:32 -0600

    Added Balloon Guiding Triggers
    Added triggers to guide the position of the inflated balloon. Also started implementing the matchstick spring.

---
+++ OneTimeUse/Assets/ventBreeze.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ventBreeze : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        //check if the collider is tagged as "Player" this time.
+        if (other.tag != "Player")
+        {
+            other.GetComponent<Rigidbody>().useGravity = false;
+
+            other.GetComponent<Rigidbody>().velocity = new Vector3(0f, 0f, 0.5f);
+
+        }
+    }
+
+        // Start is called before the first frame update
+        void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
