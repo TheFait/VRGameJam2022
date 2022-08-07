3b85076e74c0addaecd7a7ed09bfb0f5deeea95c
Jacob Fifield <43118015+Fife@users.noreply.github.com>
2022-08-07 12:47:44 -0600

    scissor animation

---
+++ OneTimeUse/Assets/scissor_trigger.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scissor_trigger : MonoBehaviour
+{
+    bool flag;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (!flag)
+        {
+            Debug.Log("Collide!");
+            GameObject obj = GameObject.Find("/scissor blades/Blade");
+
+            Animation anim = obj.GetComponent<Animation>();
+            Behaviour bhvr = (Behaviour)anim;
+
+            bhvr.enabled = true;
+
+            anim.Play("scissor");
+
+            obj = GameObject.Find("/Blowing ball/Sphere");
+            Rigidbody gameObjectsRigidBody = obj.AddComponent<Rigidbody>(); // Add the rigidbody.
+            gameObjectsRigidBody.mass = 5;
+            flag = true;
+        }
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
